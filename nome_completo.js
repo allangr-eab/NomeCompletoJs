e2ac501a4fbb825c72fcbb80ad538b7a5f17a647
anisio silva santos <allangabrielrvsantos@yahoo.com.br>
2021-06-24 12:05:18 -0300

    nome completo

---
+++ nome_completo.js
@@ -0,0 +1,13 @@
+let nome= prompt("Digite seu nome");
+
+let sobrenome= prompt("Digite seu sobrenome");
+
+let NomeCompleto1= 'Nome Completo:  ${nome}   ${sobrenome}.'
+
+alerta(NomeCompleto1);
+
+let sobrenome2= sobrenome. toUpperCase()
+
+let NomeCompleto2= 'Nome de catálogo:  ${sobrenome2}   ${nome}.'
+
+alerta(NomeCompleto2);
